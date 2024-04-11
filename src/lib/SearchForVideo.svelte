a8db149226f02a9e2799d72947d67ea45a70afd5
mrskyeA <115604996+mrskyeA@users.noreply.github.com>
2024-04-11 04:21:46 +0000

    first commit

---
+++ src/lib/SearchForVideo.svelte
@@ -0,0 +1,10 @@
+<div>
+    <form>
+        <label for="youtubeSearch">YouTube Video/Song Name:</label>
+        <input type = "text" name="youtubeSearch">
+        <input type = "submit" value = "Search">
+    </form>
+</div>
+<style>
+    
+</style>
