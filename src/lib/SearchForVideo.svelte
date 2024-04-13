613bc5b6f996e44570205470de152a3818a0a7ec
mrskyeA <115604996+mrskyeA@users.noreply.github.com>
2024-04-13 06:22:25 +0000

    good  code

--- src/lib/SearchForVideo.svelte
+++ src/lib/SearchForVideo.svelte
@@ -4,7 +4,37 @@
         <input type = "text" name="youtubeSearch">
         <input type = "submit" value = "Search">
     </form>
+    <ul>
+        <li>
+            <img src = "https://i.ytimg.com/vi/GgGPuuGNPAE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLC53yjMrVtkhcBJRdvh86Zc5VJklw"/>
+            <div class = "videoText">
+                <div class = "videoTitle">Take It Off</div>
+                <div class = "creatorName">kesha</div>
+            </div>
+            
+        </li>
+    </ul>
 </div>
 <style>
     
+    li{
+        display:flex;
+        
+        padding:5px;
+        border-radius: 5px;
+        background-color: grey;
+    }
+    img{
+        align-self: center;
+        object-fit: contain;
+        float:left;
+    }
+    .videoText{
+        margin-left:5px;
+    }
+    .creatorName{
+        margin-top:8px;
+        color:white;
+        font-size:12px;
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
+	import SearchForVideo from "$lib/SearchForVideo.svelte";
+
 	
 </script>
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
+<div>
+	<SearchForVideo/>
+</div>
